0631120b226efe2e9ecce80fbae747ca58677dad
adurbin <adurbin@694b9286-2a4a-0410-bdee-d9d00b442da2>
2008-07-30 15:49:49 +0000

    Only define __USE_GNU macro if it is not already defined.

--- misc.c
+++ misc.c
@@ -30,7 +30,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include "commands.h"
+#ifndef __USE_GNU
 #define __USE_GNU
+#endif
 #include <sched.h>
 
 /* Helper function to set the affinity of the process to a given cpu. */
