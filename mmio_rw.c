8b01f69ba11121bd5c065097d7f6bfe139ef38b6
adurbin <adurbin@694b9286-2a4a-0410-bdee-d9d00b442da2>
2008-08-08 17:04:03 +0000

    Add mmio_dump command.
    Implement a mmio_dump command that will print out the number of requested
    bytes in 32-bit values. If the number of bytes requested is not a multiple of
    4, it prints the remaining bytes as single bytes.
    
    open_mapping() was modified to take a parameter of the desired bytes requested
    to map. struct mmap_info was also modified to keep track of the correct length
    of the mapping.

--- mmio_rw.c
+++ mmio_rw.c
@@ -36,13 +36,14 @@ struct mmap_info {
 	volatile void *mem;
 	int off;
 	size_t pgsize;
+	size_t length;
 	uint64_t addr;
 };
 
 /* open /dev/mem and mmap the address specified in mmap_addr. return 1 on
  * success, 0 on failure. */
 static int
-open_mapping(struct mmap_info *mmap_addr, int flags)
+open_mapping(struct mmap_info *mmap_addr, int flags, unsigned bytes)
 {
 	int prot;
 
@@ -66,7 +67,11 @@ open_mapping(struct mmap_info *mmap_addr, int flags)
 		prot = PROT_WRITE;
 	}
 
-	mmap_addr->mem = mmap(NULL, mmap_addr->pgsize*2, prot, MAP_SHARED,
+	/* The length is comprised of the bytes requested including the partial
+	 * number of bytes that do not make up a full page. */
+	mmap_addr->length = bytes + mmap_addr->off;
+
+	mmap_addr->mem = mmap(NULL, mmap_addr->length, prot, MAP_SHARED,
 	           mmap_addr->fd, mmap_addr->addr);
 
 	if (!mmap_addr->mem || mmap_addr->mem == MAP_FAILED) {
@@ -81,7 +86,7 @@ open_mapping(struct mmap_info *mmap_addr, int flags)
 static void
 close_mapping(struct mmap_info *mmap_addr)
 {
-	munmap((void *)mmap_addr->mem, mmap_addr->pgsize*2);
+	munmap((void *)mmap_addr->mem, mmap_addr->length);
 	close(mmap_addr->fd);
 }
 
@@ -94,7 +99,7 @@ mmio_read_x(int argc, const char *argv[], const struct cmd_info *info)
 
 	mmap_addr.addr = strtoull(argv[1], NULL, 0);
 
-	if (open_mapping(&mmap_addr, O_RDONLY) < 0) {
+	if (open_mapping(&mmap_addr, O_RDONLY, sizeof(data)) < 0) {
 		return -1;
 	}
 
@@ -136,7 +141,7 @@ mmio_write_x(int argc, const char *argv[], const struct cmd_info *info)
 	mmap_addr.addr = strtoull(argv[1], NULL, 0);
 	ldata = strtoul(argv[2], NULL, 0);
 
-	if (open_mapping(&mmap_addr, O_RDWR) < 0) {
+	if (open_mapping(&mmap_addr, O_RDWR, sizeof(data)) < 0) {
 		return -1;
 	}
 
@@ -166,8 +171,73 @@ mmio_write_x(int argc, const char *argv[], const struct cmd_info *info)
 	return ret;
 }
 
+static int
+mmio_dump(int argc, const char *argv[], const struct cmd_info *info)
+{
+	unsigned long bytes_to_dump;
+	unsigned long bytes_left;
+	struct mmap_info mmap_addr;
+	uint32_t *addr;
+	uint64_t desired_addr;
+	int fields_on_line;
+
+	desired_addr = strtoull(argv[1], NULL, 0);
+	bytes_to_dump = strtoul(argv[2], NULL, 0);
+
+	mmap_addr.addr = desired_addr;
+	if (open_mapping(&mmap_addr, O_RDONLY, bytes_to_dump) < 0) {
+		return -1;
+	}
+
+	addr = (void *)mmap_addr.mem + mmap_addr.off;
+	bytes_left = bytes_to_dump;
+
+	fields_on_line = 0;
+	while (bytes_left) {
+		int bytes_printed = sizeof(*addr);
+		/* Print out the current address. */
+		if (!fields_on_line) {
+			fprintf(stdout, "%p:", (void *)(long)desired_addr);
+		}
+
+		/* Print out the leftover bytes. */
+		if (bytes_left < sizeof(*addr)) {
+			unsigned char *ptr = (unsigned char *)addr;
+			fprintf(stdout, " 0x%02x", *ptr);
+			/* Adjust the working pointer and the bytes_printed */
+			addr = (typeof(addr))++ptr;
+			bytes_printed = sizeof(*ptr);
+		} else {
+			fprintf(stdout, " 0x%08x", *addr);
+			addr++;
+		}
+
+		/* Keep track of statistics. */
+		bytes_left -= bytes_printed;
+		desired_addr += bytes_printed;
+
+		/* Default to printing out 4 sets of 32-bit values. */
+		fields_on_line = (fields_on_line + 1) % 4;
+
+		/* Handle the new line once we are field 0 again. */
+		if (!fields_on_line) {
+			fprintf(stdout, "\n");
+		}
+	}
+
+	/* Print newline if we stopped printing in the middle of a line. */
+	if (fields_on_line) {
+		fprintf(stdout, "\n");
+	}
+
+	close_mapping(&mmap_addr);
+
+	return 0;
+}
+
 MAKE_PREREQ_PARAMS_FIXED_ARGS(rd_params, 2, "<addr>", 0);
 MAKE_PREREQ_PARAMS_FIXED_ARGS(wr_params, 3, "<addr> <value>", 0);
+MAKE_PREREQ_PARAMS_FIXED_ARGS(dump_params, 3, "<addr> <num_bytes>", 0);
 
 #define MAKE_MMIO_READ_CMD(size_) \
 	MAKE_CMD_WITH_PARAMS(mmio_read ##size_, &mmio_read_x, &size ##size_, \
@@ -183,6 +253,7 @@ static const struct cmd_info mmio_cmds[] = {
 	MAKE_MMIO_RW_CMD_PAIR(8),
 	MAKE_MMIO_RW_CMD_PAIR(16),
 	MAKE_MMIO_RW_CMD_PAIR(32),
+	MAKE_CMD_WITH_PARAMS(mmio_dump, &mmio_dump, NULL, &dump_params)
 };
 
 MAKE_CMD_GROUP(MMIO, "commands to access memory mapped address spaces",
